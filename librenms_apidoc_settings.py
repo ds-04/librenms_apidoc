804a2581bf641192b99b41648061de89b0aa8680
David Simpson <31688862+ds-04@users.noreply.github.com>
2022-10-28 16:27:24 +0100

    Create librenms_apidoc_settings.py

---
+++ librenms_apidoc_settings.py
@@ -0,0 +1,10 @@
+# Constants/Parameters, modify these
+
+#GLOBAL EXCLUDE TUPLE - ENTRIES CASE SENSITIVE
+EXCLUDE_GROUPS=()
+#API TOKEN
+TOKEN=''
+#LIBRENMS HOST
+MON_HOST='https://somehost.somedomain'
+#OUTPUT
+TABLE_FORMAT='rst' #from tabulate use rst,csv etc.
