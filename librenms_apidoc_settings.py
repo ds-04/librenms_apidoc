82a9b6482c11f6216ab50a09b911bb8c4018ff25
David Simpson <31688862+ds-04@users.noreply.github.com>
2022-10-30 15:54:26 +0000

    Update librenms_apidoc_settings.py
    add OUTPUT_FILE_NAME and VMWARE_SERIAL_REPLACE

--- librenms_apidoc_settings.py
+++ librenms_apidoc_settings.py
@@ -8,3 +8,7 @@ TOKEN=''
 MON_HOST='https://somehost.somedomain'
 #OUTPUT
 TABLE_FORMAT='rst' #from tabulate use rst,csv etc.
+#OUTPUT FILE
+OUTPUT_FILE_NAME=str("librenms_apidoc_output."+TABLE_FORMAT)
+#VMWARE specific
+VMWARE_SERIAL_REPLACE='TRUE'
